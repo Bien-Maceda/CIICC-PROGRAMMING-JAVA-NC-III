92292ae61ad85e5db134002d3ee39820e9796a7f
Bien-Maceda <111550440+Bien-Maceda@users.noreply.github.com>
2026-07-20 08:08:22 +0000

    task1

--- TASKS/task1.java
+++ TASKS/task1.java
@@ -2,6 +2,6 @@ package TASKS;
 
 public class task1 {
     public static void main(String[] args) {
-        System.out.println(:"Kamusta Mundo!"); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
